0aff427df53c124cf52dcde025fc464b537902a6
lizhengxiang <1065482100@qq.com>
2016-10-21 21:58:42 +0800

    第一个ｊａｖａ程序

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+class Number{
+    int i;
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Number n1 = new Number();
+        Number n2 = new Number();
+        n1.i = 20;
+        n2.i = 80;
+        System.out.print(n1.i);
+        System.out.print(n2.i);
+    }
+}
