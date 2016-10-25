336c2cfbbaee7fc2a1e76c1bd9ccbea2c1ba686e
lizhengxiang <1065482100@qq.com>
2016-10-25 23:02:26 +0800

    xxx

--- src/Dinner.java
+++ src/Dinner.java
@@ -1,12 +1,12 @@
 /**
  * Created by lizhengxiang on 16-10-25.
  */
+
 public class Dinner {
     public Dinner() {
         System.out.println("Dinner constructor");
     }
     public static void main(String[] args) {
-      c05.dessert.Cookie x = new c05.dessert.Cookie();
-        //! x.foo(); // Can't access
+
     }
 }
