4975769ecef5d6fb1556ebdc9b95f2a1d001a773
lizhengxiang <1065482100@qq.com>
2016-10-25 08:23:48 +0800

    import package

---
+++ src/Cookie.java
@@ -0,0 +1,10 @@
+/**
+ * Created by lizhengxiang on 16-10-25.
+ */
+package c05.dessert;
+public class Cookie {
+    public Cookie() {
+        System.out.println("Cookie constructor");
+    }
+    void foo() { System.out.println("foo"); }
+}
